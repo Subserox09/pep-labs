ecf8be13233ec6b3693f7e56d585315ab41aafe4
Subserox09 <adekunle.kukoyi@outlook.com>
2022-12-31 05:21:54 +0000

    Saved progress on IntroToOOP 2022-12-31 05:21:54 +0000

--- IntroToOOP/src/main/java/Bird.java
+++ IntroToOOP/src/main/java/Bird.java
@@ -45,6 +45,11 @@ public class Bird {
      *  * greater than or equal to 3.
      */
     public String sound(){
+        if(this.ageMonths < 3){
+            return "Tweet";
+        }else if(this.ageMonths >= 3){
+            return "Chirp";
+        }
         return "";
     }
 

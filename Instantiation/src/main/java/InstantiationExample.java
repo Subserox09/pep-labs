aea6de70bd2c8e092ae1142f7a3dd3dc41dba7b4
Subserox09 <adekunle.kukoyi@outlook.com>
2022-12-31 05:31:58 +0000

    Saved progress on Instantiation 2022-12-31 05:31:58 +0000

--- Instantiation/src/main/java/InstantiationExample.java
+++ Instantiation/src/main/java/InstantiationExample.java
@@ -18,6 +18,7 @@ public class InstantiationExample {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Object damon = new Object();
+        return damon;
     }
 }

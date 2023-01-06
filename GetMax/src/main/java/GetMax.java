604fce2fcd3b7db2280dbbeeda93f920cdf352fb
Subserox09 <adekunle.kukoyi@outlook.com>
2023-01-06 15:39:39 +0000

    Saved progress on GetMax 2023-01-06 15:39:39 +0000

--- GetMax/src/main/java/GetMax.java
+++ GetMax/src/main/java/GetMax.java
@@ -1,3 +1,8 @@
+import org.hamcrest.core.IsNot;
+import org.hamcrest.core.IsNull;
+
+import java.util.Arrays;
+
 public class GetMax {
     /**
      * This method should return the max value of arr (the largest int.)
@@ -7,6 +12,25 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int greater1 = 0;
+        int greater2= 0;
+        int greater3 =0;
+
+        for(int i =0; i < arr.length; i++){
+            if((i+1) != arr.length){
+                if((arr[i] < arr[i+1]) ) {//&& ((i+1) != arr.length)
+                    greater1 = arr[i+1];
+                }else if(arr[i] > arr[i+1]){
+                    greater2 = arr[i];
+                }
+            }
+
+            if(greater1 > greater2){
+                greater3 = greater1;
+            }else if(greater1 < greater2){
+                greater3 = greater2;
+            }
+        }
+        return greater3;
     }
 }

e35955e02c5f0020ba4d6d9c0ecfa061d0eccacb
Subserox09 <adekunle.kukoyi@outlook.com>
2023-01-10 01:42:00 +0000

    Saved progress on Math 2023-01-10 01:42:00 +0000

--- SelectAll/src/main/java/GetAllSongs.java
+++ SelectAll/src/main/java/GetAllSongs.java
@@ -41,10 +41,23 @@ public class GetAllSongs {
         try {
             Connection connection = ConnectionUtil.getConnection();
             Statement s = connection.createStatement();
-            ResultSet rs =s.executeQuery(sql);
+            
+            s.executeUpdate(sql);
+            
+/*
+            String sql1 = "CREATE TABLE song (Title varchar(100), Artist varchar(100));";
+            String sql2 = "INSERT INTO song VALUES ('Let it be', 'Beatles');";
+            String sql3 = "INSERT INTO song VALUES ('Hotel California', 'Eagles');";
+            String sql4 = "INSERT INTO song VALUES ('Kashmir', 'Led Zeppelin');";
+            System.out.println("Reached here at least?");
+            s.executeUpdate(sql1 + sql2 + sql3 + sql4);
 
-            while(rs.next()){
+            
+*/
+            ResultSet rs =s.executeQuery(sql);
+             while(rs.next()){
                 songs.add(new Song(rs.getString(1), rs.getString(2)));
+                
             }
         } catch (SQLException e) {
             System.out.println("problem1: " + e.getMessage() + '\n');

bf9a2eb83a67e2c21fb5f784c7ebe4c5a94b9938
Subserox09 <adekunle.kukoyi@outlook.com>
2023-02-11 18:17:01 +0000

    Saved progress on FlightTracker 2023-02-11 18:17:01 +0000

--- FlightTracker/src/main/java/Application/DAO/FlightDAO.java
+++ FlightTracker/src/main/java/Application/DAO/FlightDAO.java
@@ -66,11 +66,13 @@ public class FlightDAO {
         Connection connection = ConnectionUtil.getConnection();
         try {
             //Write SQL logic here
-            String sql = "select * from flight where flight_id =";
+            String sql = "select * from flight where flight_id =?";
             
             PreparedStatement preparedStatement = connection.prepareStatement(sql);
+           
 
             //write preparedStatement's setString and setInt methods here.
+            preparedStatement.setInt(0,id);
 
             ResultSet rs = preparedStatement.executeQuery();
             while(rs.next()){
@@ -108,10 +110,12 @@ public class FlightDAO {
         try {
             //Write SQL logic here. When inserting, you only need to define the departure_city and arrival_city
             //values (two columns total!)
-            String sql = "change me" ;
+            String sql = "insert into flight (departure_city,arrival_city) values (?,?);" ;
             PreparedStatement preparedStatement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
             //write preparedStatement's setString and setInt methods here.
+            preparedStatement.setString(1,flight.getDeparture_city());
+            preparedStatement.setString(2,flight.getArrival_city());
 
 
             preparedStatement.executeUpdate();

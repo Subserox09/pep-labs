54852b795e25f77fe493257310aa83fd12baed68
Subserox09 <adekunle.kukoyi@outlook.com>
2023-02-21 16:48:30 +0000

    Saved progress on CombineArrays 2023-02-21 16:48:30 +0000

--- CombineArrays/src/main/java/CombineArrays.java
+++ CombineArrays/src/main/java/CombineArrays.java
@@ -8,22 +8,29 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        int sum1 = arr1.length;
+        int sum2 = arr2.length;
 
-        int[] newArr = new int[arr1.length + arr2.length];
+        int[] newArr = new int[sum1 + sum2];
 
         int value =0;
 
         while(value < newArr.length){
 
-        }
-        for(int i = 0; i < arr1.length; i++){
-            newArr[value] = arr1[i];
-            value+=1;
-        }
+        
+            for(int i = 0; i < arr1.length; i++){
+                newArr[value] = arr1[i];
+                //System.out.println("arr1 index " + i + " : " + arr1[i]);
+                //System.out.println("newArr index " + value + " : " + newArr[value]);
+                value+=1;
+            }
 
-        for(int j = 0; j < arr2.length; j++){
-            newArr[value] = arr1[j];
-            value+=1;
+            for(int j = 0; j < arr2.length; j++){
+                newArr[value] = arr2[j];
+                //System.out.println("arr2 index " + j + " : " + arr2[j]);
+                //System.out.println("newArr index " + value + " : " + newArr[value]);
+                value+=1;
+            }
         }
         return newArr;
     }
